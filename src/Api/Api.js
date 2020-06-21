e9190778140ba566bdae60666761a3e01d50df04
Tae Heon Kim <top1kth98@gmail.com>
2020-06-21 17:10:50 +0900

    게시판 UI 작업

--- src/Api/Api.js
+++ src/Api/Api.js
@@ -25,6 +25,27 @@ export default class Api {
     signInWithGoogle();
   }
   
+  //유저 정보 가져오기
+  changeUserStatus() {
+    firebase.auth().onAuthStateChanged(function(user) {
+      if (user) {
+        // User is signed in.
+        var userName = user.displayName;
+        var userEmail = user.email;
+        var emailVerified = user.emailVerified;
+        var photoURL = user.photoURL;
+        var isAnonymous = user.isAnonymous;
+        var uid = user.uid;
+        var providerData = user.providerData;
+        // ...
+        console.log(emailVerified);
+        // this.localLogin(userName, userEmail)
+      } else {
+
+      }
+    });
+  }
+  
   //이메일로 로그인
   signInWithEmail(email, password){
     firebase.auth().signInWithEmailAndPassword(email, password).catch(function(error) {
@@ -59,26 +80,6 @@ export default class Api {
     });
   }
 
-  //유저 정보 가져오기
-  changeUserStatus() {
-    firebase.auth().onAuthStateChanged(function(user) {
-      if (user) {
-        // User is signed in.
-        var userName = user.displayName;
-        var userEmail = user.email;
-        var emailVerified = user.emailVerified;
-        var photoURL = user.photoURL;
-        var isAnonymous = user.isAnonymous;
-        var uid = user.uid;
-        var providerData = user.providerData;
-        // ...
-        console.log(emailVerified);
-        // this.localLogin(userName, userEmail)
-      } else {
-
-      }
-    });
-  }
 
 
   postBoard(College,text) {
--- src/App.js
+++ src/App.js
@@ -1,35 +1,82 @@
 import React, { useState, useEffect } from 'react';
 import './App.css';
-import Api from './Api/Api'
+import Api from './Api/Api';
+import {Board} from './Board';
 
 function App() {
   const [boardList, setBoardList] = useState([]);
+  const [chatMessage, setChatMessage] = useState('');
+  const [chatCollege, setChatCollege] = useState('');
 
   const api = new Api();
 
-  const postBoard = (college, text) => {
-    api.postBoard(college, text)
+  // 시간 나타내기!
+  function timeConverter(UNIX_timestamp){
+    var a = new Date(UNIX_timestamp);
+    var months = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
+    var year = a.getFullYear();
+    var month = months[a.getMonth()];
+    var date = a.getDate();
+    var hour = a.getHours();
+    var min = a.getMinutes();
+    var sec = a.getSeconds();
+    var time = date + ' ' + month + ' ' + year + ' ' + hour + ':' + min + ':' + sec ;
+    return time;
   }
 
-  const loadBoardList = () => {
+
+  // 응원글 reload
+  const loadBoardList = async() => {
     // api.loadBoardList()
-    api.loadBoardList().then((res) => {
-      setBoardList(res)
-    })
-  }
 
+    await api.loadBoardList().then((res) => {
+      // for (let i=0; i<res.length; i++){
+      //   const timestamp = res[i].date;
+      //   res[i].date = timeConverter(timestamp);
+      // };
+      setBoardList(res)
+    });
+  };
+  
   useEffect(() => {
     loadBoardList();
-  }, [])
+  }, []);
+
+  // 글 작성
+  const postBoard = (college, text) => {
+    api.postBoard(college, text);
+  }
 
   return (
     <div className="App">
       <header className="App-header">
-        <div>
-          <button onClick={postBoard('농생대','힘내세요!!')}> postBoard </button>
-          <div>
-            { (boardList) ? boardList.map((board) => <div> {board.text} </div>) : <div>게시글없음.</div>}
+        <div style={{ backgroundColor: '#B2C6D9', width: '800px', height: '700px', border: '1px solid black', overflow: "scroll", overflowX: "hidden"}}>
+          <div style={{marginTop : '15px', marginBottom: '10px'}}>
+            <form>
+              <textarea id="message-box" type="text" placeholder="응원글을 적어주세요!" value={chatMessage} onChange={e => setChatMessage(e.target.value)}>{chatMessage}</textarea>
+              <select value={chatCollege} size={1}>
+                <option value="경영대">경영대</option>
+                <option value="인문대">인문대</option>
+                <option value="사회대">사회대</option>
+                <option value="자연대">자연대</option>
+                <option value="공과대">공과대</option>
+                <option value="농생대">농생대</option>
+                <option value="생활대">생활대</option>
+                <option value="음미대">음미대</option>
+                <option value="수의대">수의대</option>
+                <option value="약학대">약학대</option>
+                <option value="의대">의대</option>
+                <option value="사범대">사범대</option>
+              </select>
+              <button type="submit" id="submit-button"> 응원하기 </button>
+              <button onClick={postBoard('경영대','힘내세요!!')}>postBoard</button>
+            </form>
           </div>
+          <div>
+            { (boardList) ? boardList.map((board) => <Board board={board} key={board.Key}/>) : <div>게시글없음.</div>}
+          </div>
+        </div>
+        <div>
           <button onClick={loadBoardList}> loadBoardList! </button>
           <button onClick={api.signInWithGoogle}> Sign in with Google </button>
           <button onClick={api.signInWithEmail('wlsdnr330@snu.ac.kr','password')}> Sign in with Email </button>

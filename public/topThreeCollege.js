328c062cbb611965d66044e9a845f0050a35ae8d
JinWook-SNU <wlsdnr330@snu.ac.kr>
2020-06-25 02:18:29 +0900

    물병 탑3 적용하기에서 막힘

--- public/topThreeCollege.js
+++ public/topThreeCollege.js
@@ -1,10 +1,11 @@
+import Api from '../src/Api/Api'
 function topThreeCollege() {
   const api = new Api();
+  console.log('333')
 
   api.getTopThree().then((res)=>{
     var obj_length = Object.keys(res).length;
     console.log(obj_length);
     return obj_length
   }
-
 }
